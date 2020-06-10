7f6cd5f360317d645e4989131c9b87378fd8dcf4
av <45238682+avxlji@users.noreply.github.com>
2020-06-10 12:18:32 -0400

    update

--- client/src/components/layout/Spinner.js
+++ client/src/components/layout/Spinner.js
@@ -1,11 +1,11 @@
-import React from "react";
-import img from "../assets/loading.gif";
-import "../../styles/Spinner.css";
+import React from 'react';
+// import img from "../assets/loading.gif";
+import '../../styles/Spinner.css';
 
 const Spinner = () => {
   return (
     <div id="loading-container">
-      <img src={img} id="loading-gif"></img>
+      {/* <img src={img} id="loading-gif"></img> */}
     </div>
   );
 };
